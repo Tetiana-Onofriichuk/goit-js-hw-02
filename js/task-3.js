63a3c3242c26baba24ceb8de340cca116304035a
Tetiana <Korostijtatiana@gmail.com>
2025-03-30 14:51:49 +0300

    add files

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,30 @@
 "use strict";
 
-function getElementWidth(content, padding, border) {
-  const contenWidth = Number.parseFloat(content);
-  const paddingWidth = Number.parseFloat(padding);
-  const borderWidth = Number.parseFloat(border);
-
-  return `${contenWidth + paddingWidth * 2 + borderWidth * 2}`;
+function checkForSpam(message) {
+  message = message.toLowerCase();
+  return message.includes("spam") || message.includes("sale");
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+function getSubscriptionPrice(type) {
+  switch (type) {
+    case "starter":
+      return 0;
+      break;
+    case "professional":
+      return 20 + " dollars";
+      break;
+    case "organization":
+      return 50;
+      break;
+    default:
+      return "Invalid subscription type!";
+  }
+}
